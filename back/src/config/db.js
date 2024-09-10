8449ea7bd933167a83c2822d3912dae4ef6c29f0
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-10 13:33:29 +0200

    Update db.js

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
    try {
-    await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    await mongoose.connect(process.env.DB_URL)
     console.log("Conectado con éxito a la BBDD");
    } catch (error) {
     console.log("Error al conectar a la BBDD");
