f14e5164a1f0904ecb2126372b24abc88db76770
Eduardo <148449539+SirEduardo@users.noreply.github.com>
2024-09-12 10:08:17 +0200

    Update index.js

--- back/index.js
+++ back/index.js
@@ -18,7 +18,7 @@ cloudinary.config({
 
 app.use(express.json())
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || "https://project-10-front.vercel.app/", 
+    origin: process.env.FRONTEND_URL || "http://localhost:3000", 
     optionsSuccessStatus: 200
   };
   
